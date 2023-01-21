026ee83d8a32749f132c8916d3a946dbe0c66f44
Pedro Freddi <pdafreddi@gmail.com>
2023-01-21 17:00:19 -0300

    Disable caching for API responses

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const nocache = require("nocache");
 const productsRouter = require("./products/productsRouter.js");
 const usersRouter = require("./users/usersRouter.js");
 
 const router = express.Router();
 
+router.use(nocache());
 router.use("/products", productsRouter);
 router.use("/users", usersRouter);
 
