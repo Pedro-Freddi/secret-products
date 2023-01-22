14f66225b94f565d61a0c0198e62c0ddb25b6f0a
Pedro Freddi <pdafreddi@gmail.com>
2023-01-22 19:58:46 -0300

    Rename router files to keep naming convention consistent

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const nocache = require("nocache");
-const productsRouter = require("./products/productsRouter.js");
-const usersRouter = require("./users/usersRouter.js");
+const productsRouter = require("./products/products.router.js");
+const usersRouter = require("./users/users.router.js");
 
 const router = express.Router();
 
+// Disable caching for API endpoints
 router.use(nocache());
+
 router.use("/products", productsRouter);
 router.use("/users", usersRouter);
 
--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("*", (req, res) => {
 });
 
 // Initialize server
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
